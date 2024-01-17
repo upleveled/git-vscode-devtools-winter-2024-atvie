da627bcff431299568f3177a2b6ec67297f470d6
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-01-17 15:09:44 +0100

    Create animals array

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function greet2(name) {
 
 greet();
 greet2('Victor');
+
+const animals = [
+  { id: 1, firstName: 'Ben', type: 'Elephant', accessory: 'home' },
+  { id: 2, firstName: 'Jack', type: 'Cat', accessory: 'comb' },
+  { id: 3, firstName: 'Seed', type: 'Dog', accessory: 'bike' },
+  { id: 4, firstName: 'Jenny', type: 'Rabbit', accessory: 'home' },
+  { id: 5, firstName: 'Mart', type: 'Cat', accessory: 'car' },
+];
